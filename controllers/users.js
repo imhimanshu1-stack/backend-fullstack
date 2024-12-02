6e4a374d48e04f55715d831b48a6ece257e5a751
imhimanshu1-stack <himanshuverma131213@gmail.com>
2024-12-02 17:26:26 +0530

    login and register api

--- controllers/users.js
+++ controllers/users.js
@@ -27,13 +27,34 @@ exports.register = async (req, res) => {
     const result = await db
       .collection("users")
       .insertOne({ email, password: hashedPassword });
-    res
-      .status(201)
-      .json({
-        message: "User registered successfully.",
-        userId: result.insertedId,
-      });
+    res.status(201).json({
+      message: "User registered successfully.",
+      userId: result.insertedId,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await db.collection("users").findOne({ email });
+
+    if (!user) {
+      return res.status(200).json({ message: "user not found" });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "password is not correct" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return res.json({ message: "login was successful", token: token });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
-const { register } = require("../controllers/users");
+const { register, login } = require("../controllers/users");
 
 const router = require("express").Router();
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
