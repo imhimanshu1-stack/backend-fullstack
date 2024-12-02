4f2eee862e661b20818cdafa6d49657558af59ab
imhimanshu1-stack <himanshuverma131213@gmail.com>
2024-12-02 17:38:46 +0530

    cors implemented

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ const uri = "mongodb://localhost:27017";
 require("dotenv").config({ path: "./config/config.env" });
 const users = require("./routes/user");
 const products = require("./routes/product");
-
 const path = require("path");
+const cors = require("cors");
 const dbName = "ecom";
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "PUT", "DELETE", "PATCH", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/v1", users);
 app.use("/api/v1", products);
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
